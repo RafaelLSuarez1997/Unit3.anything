0c4dec7f1e068207f9c9596b2b6a0b0d64566118
Brian Capraro <Briancapraro0228@gmail.com>
2023-10-10 15:51:04 -0400

    finished puppy data with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-//import { useState } from 'react'
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log("puppyList: ", puppyList);
 
   return (
     <>
-     <h1>Goodbye world</h1>
+    <div>
+    { 
+   puppies.map((puppy) => {
+     return <p>{puppy.name}</p>
+   })
+}
+    </div>
     </>
   )
 }
